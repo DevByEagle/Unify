b7f991110e00b97f6d00b03aa0ceacb5d533ac25
Aaron Thompson <athompsonbriley.56@gmail.com>
2025-06-12 20:51:58 -0400

    Add project files.

---
+++ UnifyEditor/GameProject/NewProject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnifyEditor.Common;
+
+namespace UnifyEditor.GameProject
+{
+  public class ProjectTemplate
+  {
+    public string ProjectType { get; set; }
+  }
+
+  class NewProject : ViewModelBase
+  {
+
+  }
+}
